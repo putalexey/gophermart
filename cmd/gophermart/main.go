b6ad223f9141309ae226b79bbf5ed574cdd36bfa
Putintcev Alexey <putalexey@gmail.com>
2022-02-09 03:07:40 +0700

    add Api module add graceful shutdown

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,23 +1,30 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"flag"
 	"github.com/caarlos0/env/v6"
+	"github.com/putalexey/gophermart/app/api"
+	"go.uber.org/zap"
 	"log"
+	"os"
+	"os/signal"
 	"regexp"
+	"sync"
+	"syscall"
 )
 
 var addressPattern = `^.+?:\d{1,5}$`
 
-type EnvConfig struct {
+type AppConfig struct {
 	Address              string `env:"RUN_ADDRESS"`
 	AccrualSystemAddress string `env:"ACCRUAL_SYSTEM_ADDRESS"`
 	DatabaseDSN          string `env:"DATABASE_URI"`
 }
 
 func main() {
-	cfg := EnvConfig{
+	cfg := AppConfig{
 		Address:              "localhost:8000",
 		AccrualSystemAddress: "",
 		DatabaseDSN:          "",
@@ -31,9 +38,54 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
+
+	logger := makeLogger()
+	defer logger.Sync() // flushes buffer, if any
+
+	logger.Infow(
+		"starting gophermart API",
+		"addr", cfg.Address,
+		"accrual_addr", cfg.AccrualSystemAddress,
+	)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	go func() {
+		// Launch Gin and
+		// handle potential error
+		api.Api{
+			Logger:         logger,
+			Address:        cfg.Address,
+			AccrualAddress: cfg.AccrualSystemAddress,
+			DatabaseDSN:    cfg.DatabaseDSN,
+		}.Run(ctx)
+
+		wg.Done()
+	}()
+
+	quit := make(chan os.Signal)
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	logger.Info("Gracefully shutting down server...")
+	cancel()
+
+	// wait for go routine with api ends shutdown process
+	wg.Wait()
+
+	logger.Info("Successfully shutdown")
 }
 
-func checkConfig(cfg EnvConfig) error {
+func makeLogger() *zap.SugaredLogger {
+	baseLogger, _ := zap.NewProduction()
+	//baseLogger, _ := zap.NewDevelopment()
+	return baseLogger.Sugar()
+}
+
+func checkConfig(cfg AppConfig) error {
 	if cfg.Address == "" {
 		return errors.New("не указан адрес и порт запуска сервиса")
 	}
@@ -55,7 +107,7 @@ func checkConfig(cfg EnvConfig) error {
 	return nil
 }
 
-func parseFlags(cfg *EnvConfig) {
+func parseFlags(cfg *AppConfig) {
 	addressFlag := flag.String("a", "", "адрес и порт запуска сервиса")
 	databaseDSNFlag := flag.String("d", "", "адрес подключения к базе данных")
 	accrualAddressFlag := flag.String("r", "", "адрес системы расчёта начислений")
--- go.mod
+++ go.mod
@@ -2,4 +2,36 @@ module github.com/putalexey/gophermart
 
 go 1.17
 
-require github.com/caarlos0/env/v6 v6.9.1 // indirect
+require (
+	github.com/caarlos0/env/v6 v6.9.1
+	github.com/gin-gonic/gin v1.7.7
+	go.uber.org/zap v1.21.0
+)
+
+require (
+	github.com/gin-contrib/gzip v0.0.5 // indirect
+	github.com/gin-contrib/sessions v0.0.4 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/gin-contrib/zap v0.0.2 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.10.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/gorilla/context v1.1.1 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/gorilla/sessions v1.2.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/ugorji/go/codec v1.2.6 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
+	golang.org/x/crypto v0.0.0-20220208050332-20e1d8d225ab // indirect
+	golang.org/x/sys v0.0.0-20220207234003-57398862261d // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
