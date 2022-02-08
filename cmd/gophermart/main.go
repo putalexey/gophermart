8ea453e3b3e48003b5c76a1b3e5e5832f404ef36
Putintcev Alexey <putalexey@gmail.com>
2022-02-09 00:10:54 +0700

    add reading app configuration from env and flags

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,62 @@
 package main
 
-func main() {}
+import (
+	"errors"
+	"flag"
+	"github.com/caarlos0/env/v6"
+	"log"
+)
+
+type EnvConfig struct {
+	Address              string `env:"RUN_ADDRESS"`
+	AccrualSystemAddress string `env:"ACCRUAL_SYSTEM_ADDRESS"`
+	DatabaseDSN          string `env:"DATABASE_URI"`
+}
+
+func main() {
+	cfg := EnvConfig{
+		Address:              "localhost:8000",
+		AccrualSystemAddress: "",
+		DatabaseDSN:          "",
+	}
+	err := env.Parse(&cfg)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	parseFlags(&cfg)
+	err = checkConfig(cfg)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+}
+
+func checkConfig(cfg EnvConfig) error {
+	if cfg.Address == "" {
+		return errors.New("не указан адрес и порт запуска сервиса")
+	}
+	if cfg.DatabaseDSN == "" {
+		return errors.New("не указан адрес подключения к базе данных")
+	}
+	if cfg.AccrualSystemAddress == "" {
+		return errors.New("не указан адрес системы расчёта начислений")
+	}
+	return nil
+}
+
+func parseFlags(cfg *EnvConfig) {
+	addressFlag := flag.String("a", "", "адрес и порт запуска сервиса")
+	databaseDSNFlag := flag.String("d", "", "адрес подключения к базе данных")
+	accrualAddressFlag := flag.String("r", "", "адрес системы расчёта начислений")
+	flag.Parse()
+
+	if *addressFlag != "" {
+		cfg.Address = *addressFlag
+	}
+	if *databaseDSNFlag != "" {
+		cfg.DatabaseDSN = *databaseDSNFlag
+	}
+	if *accrualAddressFlag != "" {
+		cfg.AccrualSystemAddress = *accrualAddressFlag
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/putalexey/gophermart
+
+go 1.17
+
+require github.com/caarlos0/env/v6 v6.9.1 // indirect
